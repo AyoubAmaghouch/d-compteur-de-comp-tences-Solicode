60b35417e2eb2bb2cb3b758016c813912f23f425
ayoub <ayoubamaghouche@gmail.com>
2025-09-19 16:37:51 +0100

    live coding

--- ex1 (Décompteur de compétences Solicode)/script.js
+++ ex1 (Décompteur de compétences Solicode)/script.js
@@ -1,9 +1,12 @@
+function nambrsc() {
 let nambrs = ["c1","c3","c2","c1","c8","c1" ];
-let choisi = "c1";
+let choisi = "c2";
 let counter = 0;
 for (let i = 0; i < nambrs.length; i++) 
     if (nambrs[i] === choisi) {
         counter++;
-         
+
     }
-    console.log("all is here   "+ counter);  
+    console.log("all is here   "+ counter);
+﻿}nambrsc();
+//end of script.js
