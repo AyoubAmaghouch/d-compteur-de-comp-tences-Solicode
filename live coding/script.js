8c5953da4f78a1aeed59eaceb90c9b54c7122a27
ayoub <ayoubamaghouche@gmail.com>
2025-09-19 16:49:35 +0100

    ain

---
+++ live coding/script.js
@@ -0,0 +1,12 @@
+function nambrsc() {
+let nambrs = ["b","c3","c2","b","c8","b" ];
+let choisi = "b";
+let counter = 0;
+for (let i = 0; i < nambrs.length; i++) 
+    if (nambrs[i] === choisi) {
+        counter++;
+
+    }
+    console.log("all is here   "+ counter);
+﻿}nambrsc();
+//end of script.js
